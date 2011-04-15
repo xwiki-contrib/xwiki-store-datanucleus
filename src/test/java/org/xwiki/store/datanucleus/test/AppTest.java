3ec5ec33e69975a1eb53569c87fbbbd60908ad15
cjdelisle <calebdelisle@lavabit.com>
2011-04-15 06:09:08 -0400

    Succeeded in dynamicly compiling, enhancing, and persisting groovy objects.

--- src/test/java/org/xwiki/store/datanucleus/test/AppTest.java
+++ src/test/java/org/xwiki/store/datanucleus/test/AppTest.java
@@ -7,9 +7,22 @@ import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 import org.apache.cassandra.service.EmbeddedCassandraService;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Before;
 import org.junit.Test;
+
+import org.codehaus.groovy.control.Phases;
+import org.codehaus.groovy.tools.GroovyClass;
+import org.codehaus.groovy.control.CompilationUnit;
+import org.codehaus.groovy.control.CompilerConfiguration;
+import org.codehaus.groovy.tools.Compiler;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.File;
+import org.apache.commons.io.IOUtils;
+import javax.jdo.JDOEnhancer;
+import groovy.lang.GroovyClassLoader;
 
 public class AppTest
 {
@@ -69,6 +82,82 @@ public class AppTest
     public void testSearchByContentAndNonIndexed() throws Exception
     {
         searchDocuments("SELECT FROM org.xwiki.store.datanucleus.test.Document WHERE content == \"ContentA\" && notIndexed == \"Hello\"");
+    }
+
+    @Test
+    public void testStoreLoadGroovyGeneratedClass() throws Exception
+    {
+        // If the package is not specified the enhancer will silently ignore the class!
+        final String script =
+            "package org.xwiki.store.datanucleus.test;\n"
+          + "import javax.jdo.annotations.IdentityType;\n"
+          + "import javax.jdo.annotations.Index;\n"
+          + "import javax.jdo.annotations.PersistenceCapable;\n"
+          + "import javax.jdo.annotations.PrimaryKey;\n"
+
+          + "@PersistenceCapable(identityType = IdentityType.APPLICATION)\n"
+          + "public class GroovyPersist\n"
+          + "{\n"
+          + "    @Index\n"
+          + "    public String author;\n"
+
+          + "    public String notIndexed;\n"
+
+          + "    @Index\n"
+          + "    public String content;\n"
+
+          + "    @PrimaryKey\n"
+          + "    @Index\n"
+          + "    public String title;\n"
+
+          + "    public String toString()\n"
+          + "    {\n"
+          + "        return author + ' | ' + notIndexed + ' | ' + content + ' | ' + title;\n"
+          + "    }\n"
+          + "}\n";
+
+        // Compile
+        final CompilationUnit cu = new CompilationUnit();
+        cu.addSource("test", script);
+        cu.compile(Phases.CLASS_GENERATION);
+        final GroovyClass gclass = (GroovyClass) cu.getClasses().get(0);
+
+        // Load
+        GroovyClassLoader loader = new GroovyClassLoader();
+        loader.defineClass(gclass.getName(), gclass.getBytes());
+
+        // Enhance!
+        final JDOEnhancer enhancer = JDOHelper.getEnhancer();
+        enhancer.setClassLoader(loader);
+        enhancer.addClass(gclass.getName(), gclass.getBytes());
+        enhancer.enhance();
+        final byte[] enhancedClass = enhancer.getEnhancedBytes(gclass.getName());
+
+        // Reload, we need a new loader since the unenhanced class is loaded already.
+        loader = new GroovyClassLoader();
+        loader.defineClass(gclass.getName(), enhancedClass);
+
+        // Create instance
+        Class groovyPersistClass = loader.loadClass(gclass.getName());
+        Object groovyPersist = groovyPersistClass.newInstance();
+        // We can't do this in java without reflection since the class doesn't exist at compile time.
+        final groovy.lang.GroovyShell gs = new groovy.lang.GroovyShell();
+        gs.setVariable("groovyPersist", groovyPersist);
+        gs.evaluate("groovyPersist.author = 'Me';\n"
+                  + "groovyPersist.notIndexed = 'Not indexed';\n"
+                  + "groovyPersist.content = 'Generated persistance capable class!';\n"
+                  + "groovyPersist.title = 'GroovyClass';\n");
+
+        // Store
+        this.manager.makePersistent(groovyPersist);
+
+        // Query
+        Collection c = (Collection)
+            this.manager.newQuery("SELECT FROM " + gclass.getName()
+                                + " WHERE title == \"GroovyClass\"").execute();
+        Assert.assertEquals(c.size(), 1);
+        Assert.assertEquals("Me | Not indexed | Generated persistance capable class! | GroovyClass",
+                            c.iterator().next().toString());
     }
 
     private void searchDocuments(final String jdoqlQuery)
